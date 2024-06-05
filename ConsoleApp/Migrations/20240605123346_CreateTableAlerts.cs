1822f677db9bfad209d17221d5bf2b89e4132d15
Vittorio GIORDANO <vittorio.giordano@alten.it>
2024-06-05 14:36:01 +0200

    Create new table Alert with migrations

---
+++ ConsoleApp/Migrations/20240605123346_CreateTableAlerts.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableAlerts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AlerDate",
+                table: "Alerts",
+                newName: "AlertDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AlertDate",
+                table: "Alerts",
+                newName: "AlerDate");
+        }
+    }
+}
