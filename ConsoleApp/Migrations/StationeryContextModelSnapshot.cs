60c1d9cbbc2b7df9b16d54e028a030a53aabbee4
acioce94 <127499776+acioce94@users.noreply.github.com>
2024-06-06 11:36:43 +0200

    RestoreMigrationAtItemTable

--- ConsoleApp/Migrations/StationeryContextModelSnapshot.cs
+++ ConsoleApp/Migrations/StationeryContextModelSnapshot.cs
@@ -22,41 +22,6 @@ namespace ConsoleApp.Migrations
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("DBLayer.Models.Alert", b =>
-                {
-                    b.Property<int>("AlertId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("AlertId"));
-
-                    b.Property<DateTime>("AlertDate")
-                        .HasColumnType("datetime");
-
-                    b.Property<string>("Email")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar");
-
-                    b.Property<string>("EmailBody")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<int>("ItemId")
-                        .HasColumnType("int");
-
-                    b.Property<int>("RefillId")
-                        .HasColumnType("int");
-
-                    b.HasKey("AlertId");
-
-                    b.HasIndex("ItemId");
-
-                    b.HasIndex("RefillId");
-
-                    b.ToTable("Alerts", (string)null);
-                });
-
             modelBuilder.Entity("DBLayer.Models.Item", b =>
                 {
                     b.Property<int>("ItemId")
@@ -98,54 +63,6 @@ namespace ConsoleApp.Migrations
                     b.HasKey("ItemId");
 
                     b.ToTable("Items", (string)null);
-                });
-
-            modelBuilder.Entity("DBLayer.Models.Refill", b =>
-                {
-                    b.Property<int>("RefillId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("RefillId"));
-
-                    b.Property<int>("Quantity")
-                        .HasColumnType("int");
-
-                    b.Property<DateTime>("RefillDate")
-                        .HasColumnType("datetime");
-
-                    b.HasKey("RefillId");
-
-                    b.ToTable("Refill");
-                });
-
-            modelBuilder.Entity("DBLayer.Models.Alert", b =>
-                {
-                    b.HasOne("DBLayer.Models.Item", "Item")
-                        .WithMany("Alerts")
-                        .HasForeignKey("ItemId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.HasOne("DBLayer.Models.Refill", "Refill")
-                        .WithMany("Alerts")
-                        .HasForeignKey("RefillId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("Item");
-
-                    b.Navigation("Refill");
-                });
-
-            modelBuilder.Entity("DBLayer.Models.Item", b =>
-                {
-                    b.Navigation("Alerts");
-                });
-
-            modelBuilder.Entity("DBLayer.Models.Refill", b =>
-                {
-                    b.Navigation("Alerts");
                 });
 #pragma warning restore 612, 618
         }
